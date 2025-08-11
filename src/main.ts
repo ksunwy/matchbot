2cb79cba4148dac41a8c04cbf7a2ad7321629135
Ксения Жукова <k.zhukova@rjtech.ru>
2025-08-11 17:04:02 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { randomUUID } from 'crypto';
+
+if (!(global as any).crypto) {
+  (global as any).crypto = {
+    randomUUID,
+  };
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
